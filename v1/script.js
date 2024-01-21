826169ba632f1aaf71bc75848ee75f6af9fc1d65
Weiqi Zhang <weiqi997998@gmail.com>
2024-01-21 16:37:43 -0500

    APIKEY

--- v1/script.js
+++ v1/script.js
@@ -22,13 +22,13 @@ loadFacts();
 
 async function loadFacts() {
   const res = await fetch(
-    "https://eznjxxwbvdhxihvjahll.supabase.co/rest/v1/facts",
+    "PROJECT URL",
     {
       headers: {
         apikey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bmp4eHdidmRoeGlodmphaGxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzMzI0NjYsImV4cCI6MjAxMzkwODQ2Nn0.j_jlmNfGeoA7VCsGWNoeWIUfI30ScAubdU3IBZxXRdc",
+          "API-KEY",
         authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6bmp4eHdidmRoeGlodmphaGxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzMzI0NjYsImV4cCI6MjAxMzkwODQ2Nn0.j_jlmNfGeoA7VCsGWNoeWIUfI30ScAubdU3IBZxXRdc",
+          "Bearer API-KEY",
       },
     }
   );
